1ce699c87de4dbf0c25221c4b9ec08346c77842b
Ravi Kant <46199672+Rvkt@users.noreply.github.com>
2024-12-10 18:01:13 +0530

    initial commit

---
+++ app/src/main/java/com/example/pgsdkpage/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pgsdkpage
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Set the activity_main.xml layout as the content view
+        setContentView(R.layout.activity_main)
+    }
+}
